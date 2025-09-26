b859e795317d29e2af0fb6cd0e0fb2f120a415b3
antiderson <andersondaniel.adfa@gmail.com>
2025-09-25 22:46:47 -0300

    hotfix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const token = localStorage.getItem("token");
+
+// se não tiver token, manda pro Login.html
+if (!token) {
+  window.location.href = "/Login.html";
+} else {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
